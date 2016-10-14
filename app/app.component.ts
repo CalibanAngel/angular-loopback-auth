c8768cc0e6d9cae7ccca146937af382da4a64d47
quief_h <quief_h@epitech.com>
2016-10-14 16:12:03 +0200

    correction

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,9 @@ import { Component }          from '@angular/core';
 @Component({
     moduleId: module.id,
     selector: 'my-app',
+    template: `
+        <h1>MyApp</h1>
+    `
 })
 export class AppComponent {
     title = 'Tour of Heroes';
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,5 @@
-import { bootstrap } from '@angular/platform-browser-dynamic';
-import { AppComponent } from './app.component';
-import { ROUTER_PROVIDERS } from '@angular/router'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS]);
+platformBrowserDynamic().bootstrapModule(AppModule);
+
