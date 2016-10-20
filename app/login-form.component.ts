9c90f28456d3c3711e000eb446e44060022229c7
quief_h <quief_h@epitech.com>
2016-10-20 11:04:24 +0200

    correction

--- app/login-form.component.ts
+++ app/login-form.component.ts
@@ -1,8 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service'
 
 @Component({
     moduleId: module.id,
     selector: 'login',
     templateUrl: 'login-form.component.html',
 })
-export class LoginFormComponent { }
+export class LoginFormComponent {
+    constructor(
+        private userService: UserService
+    ) {}
+
+    logout() {
+        this.userService.logout()
+    }
+
+    checkLogin() {
+        this.userService.checkLogin()
+    }
+}
